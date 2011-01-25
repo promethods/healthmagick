eddcbf3cae7f2afb7c74f4f6d778f25b3b48df61
Stephen <promethods@gmail.com>
2011-01-24 17:24:02 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Healthmagick::Application.config.secret_token = 'cbc84f29726544381aed36ba4083b69f4cde39d257158257c7a78e059ab7246313240a07916ec84e4a3535cc7b4ecd8435fd7cde293858420348ec2dc9422afb'
